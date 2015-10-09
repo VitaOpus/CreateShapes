ec10f203de86584cbf4cc4cac21770bbaa4141da
admin <admin@192.168.0.2>
2015-10-09 23:45:10 +0300

    Добавлен вариант v2

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,5 @@
--- Если я правильно понял суть задачи
+-- v1 Если я правильно понял суть задачи
 SELECT * FROM data GROUP BY type ORDER BY date ASC;
+
+-- v2 Скорее всего так
+SELECT * FROM ( SELECT * FROM data ORDER BY date DESC ) oder_date GROUP BY type ORDER BY date DESC
